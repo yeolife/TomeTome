f8fe544364734fc1b10e838dc8d152f242de2694
yeolife <yeo2507@naver.com>
2023-10-20 17:47:20 +0900

    Initial Commit

---
+++ TomeTome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TomeTome
+//
+//  Created by yeolife on 10/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TomeTome/TomeTomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomeTomeApp.swift
+//  TomeTome
+//
+//  Created by yeolife on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct TomeTomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
