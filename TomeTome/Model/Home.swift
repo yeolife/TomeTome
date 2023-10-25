7d51be7863498598403cb1f53caf8bd45ed9e4f3
yeolife <yeo2507@naver.com>
2023-10-25 16:18:33 +0900

    1. Home ViewModel 임시 생성 2. Tab bar 완성

---
+++ TomeTome/Model/Home.swift
@@ -0,0 +1,8 @@
+//
+//  Home.swift
+//  TomeTome
+//
+//  Created by yeolife on 10/25/23.
+//
+
+import Foundation
--- TomeTome/Screens/TomeTabViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TomeTabViewModel.swift
-//  TomeTome
-//
-//  Created by yeolife on 10/23/23.
-//
-
-import Foundation
