914cf8e4eb568bbc1fb63a0c0b4a2792eeabdeeb
yeolife <yeo2507@naver.com>
2023-10-30 20:48:33 +0900

    앱 아이콘 추가

--- TomeTome/Model/Path.swift
+++ TomeTome/Model/Path.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+enum PathType {
+    case TomeTabView
+    case CommentView
+    case ReplyView
+}
+
+final class PathViewModel: ObservableObject {
+    @Published var paths: [PathType]
+    @Published var isLogin: Bool
+    
+    init(paths: [PathType] = [],
+         isLogin: Bool = false) {
+        self.paths = paths
+        self.isLogin = isLogin
+    }
+    
+    func pathRemoveAll() {
+        paths.removeAll()
+    }
+}
---
+++ TomeTome/Screens/Comment/CommentView.swift
@@ -0,0 +1,18 @@
+//
+//  CommentView.swift
+//  TomeTome
+//
+//  Created by yeolife on 10/30/23.
+//
+
+import SwiftUI
+
+struct CommentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentView()
+}
