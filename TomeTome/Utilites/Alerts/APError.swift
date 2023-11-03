ef515ef5012aab5d07d0ac42e4f0f4974d0b363f
yeolife <yeo2507@naver.com>
2023-11-04 03:03:19 +0900

    로그인 뷰 완성 애플 로그인 일부 리팩토링

---
+++ TomeTome/Utilites/Alerts/APError.swift
@@ -0,0 +1,51 @@
+//
+//  APError.swift
+//  TomeTome
+//
+//  Created by yeolife on 11/4/23.
+//
+
+import Foundation
+
+enum SignInWithAppleError: LocalizedError {
+    case invalidCredential
+    case invalidState
+    case unableToFetchToken
+    case unableToSerializeToken
+    case unableToFindNonce
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidCredential:
+            return "잘못된 자격 증명: ASAuthorization 실패."
+        case .invalidState:
+            return "잘못된 상태: 로그인 콜백이 수신 되었지만 로그인 요청이 전송되지 않았습니다."
+        case .unableToFetchToken:
+            return "ID 토큰을 가져올 수 없습니다."
+        case .unableToSerializeToken:
+            return "데이터에서 토큰 문자열을 직렬화할 수 없습니다."
+        case .unableToFindNonce:
+            return "현재 nonce를 찾을 수 없습니다."
+        }
+    }
+}
+
+enum ServerError: LocalizedError {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "서버에 연결하는 중에 문제가 발생했습니다."
+        case .invalidResponse:
+            return "서버의 응답이 잘못되었습니다. 나중에 다시 시도해주세요."
+        case .invalidData:
+            return "서버에서 받은 데이터가 잘못되었습니다."
+        case .unableToComplete:
+            return "지금은 요청을 완료할 수 없습니다. 인터넷 연결을 확인하세요."
+        }
+    }
+}
