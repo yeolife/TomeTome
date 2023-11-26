923349d6c0c56558aa6ec031c15da469251a391d
yeolife <yeo2507@naver.com>
2023-11-26 19:23:54 +0900

    홈 뷰 완성

---
+++ TomeTome/Utilites/Manager/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  TomeTome
+//
+//  Created by yeolife on 11/26/23.
+//
+
+import Foundation
---
+++ TomeTome/Utilites/SubView/RemoteImage.swift
@@ -0,0 +1,18 @@
+//
+//  RemoteImage.swift
+//  TomeTome
+//
+//  Created by yeolife on 11/26/23.
+//
+
+import SwiftUI
+
+struct RemoteImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemoteImage()
+}
